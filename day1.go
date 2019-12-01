b89c5ac872c05c9b6e16890c72f609fe08620bcd
mojk <mi.gordani@gmail.com>
2019-12-01 15:57:25 +0100

    started on the first task

---
+++ day1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func calc(f float64) float64 {
+	return math.Round(f/3.0) - 2.0
+}
+
+func main() {
+	var sum int = 0
+	file, err := os.Open("day1.txt")
+	check(err)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+
+		if scanner.Text() == "" {
+			break
+		}
+
+		value, err := strconv.ParseFloat(scanner.Text(), 64)
+		check(err)
+		sum += int(calc(value))
+	}
+
+	file.Close()
+	fmt.Println(sum)
+}
